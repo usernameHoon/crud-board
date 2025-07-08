8632672040ba5b413f3c268a5f34bd3c1e96d184
usernameHoon <gns326@naver.com>
2025-07-08 15:24:17 +0900

    Attachment 엔티티 파일 속성 처리 로직 수정

--- backend/src/main/java/com/sideproject/board/entity/Attachment.java
+++ backend/src/main/java/com/sideproject/board/entity/Attachment.java
@@ -17,8 +17,8 @@ public class Attachment {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id; // 파일 ID
 
-  private String fileName; // 원래 파일명 (사용자에게 표시)
-  private String storedFileName; // UUID 논리 파일명 (실제 저장되는 파일명)
+  private String fileName; // 원래 파일명
+  private String storedFileName; // UUID 논리 파일명
   private String filePath; // 파일 경로
 
   @ManyToOne(fetch = FetchType.LAZY)
@@ -27,7 +27,7 @@ public class Attachment {
 
   public Attachment(String fileName, String filePath) {
     this.fileName = fileName;
-    this.storedFileName = UUID.randomUUID().toString() + "_" + fileName; // UUID 적용
+    this.storedFileName = UUID.randomUUID().toString() + "_" + fileName;
     this.filePath = filePath;
   }
 }
